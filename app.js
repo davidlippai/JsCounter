a1893621efc8ce62501eb5f71610150a1777b0c6
davidlippai <lippai28@gmail.com>
2021-12-03 18:14:22 +0100

    small changes

--- app.js
+++ app.js
@@ -4,17 +4,27 @@ let decreaseBtn = document.getElementById("decrease");
 let resetBtn = document.getElementById("reset");
 let increaseBtn = document.getElementById("increase");
 
-decreaseBtn.addEventListener('click', function(){
+decreaseBtn.addEventListener('click', function () {
   counterNumber--;
   counterValue.textContent = counterNumber;
+
 })
 
-increaseBtn.addEventListener('click', function(){
+increaseBtn.addEventListener('click', function () {
   counterNumber++;
   counterValue.textContent = counterNumber;
 })
 
-resetBtn.addEventListener('click', function(){
+resetBtn.addEventListener('click', function () {
   counterNumber = 0;
   counterValue.textContent = 0;
 })
+
+
+/*if (counterNumber > 0) {
+  counterValue.classList.add("redColor");
+} else if (counterNumber < 0 ) {
+  counterValue.classList.add("greenColor");
+} else {
+  counterValue.style.color = "black";
+}*/
